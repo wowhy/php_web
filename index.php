6420d3cdb88275304c13bcc3d3d5e85025e236ad
wowhy <hongy19890331@outlook.com>
2014-01-13 11:29:21 +0800

    clean

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
  <?php
   header("content-type:text/html;charset=UTF-8");
-  echo "<h1>Hello PHP!</h1>";
-  $host = getenv("MOPAAS_POSTGRESQL6307_HOST");
-  $port = getenv("MOPAAS_POSTGRESQL6307_PORT");
-  $dbname = getenv("MOPAAS_POSTGRESQL6307_NAME");
-  $username = getenv("MOPAAS_POSTGRESQL6307_USERNAME");
-  $password = getenv("MOPAAS_POSTGRESQL6307_PASSWORD");
+  
+  $host = getenv("MOPAAS_MYSQL6309_HOST");
+  $port = getenv("MOPAAS_MYSQL6309_PORT");
+  $dbname = getenv("MOPAAS_MYSQL6309_NAME");
+  $username = getenv("MOPAAS_MYSQL6309_USERNAME");
+  $password = getenv("MOPAAS_MYSQL6309_PASSWORD");
 ?>
 <html>
   <head>
     <title>test</title>
   </head>
   <body>
-  <?php
-    echo "<h1>host: $host</h1>";
-    echo "<h1>port: $port</h1>";
-    echo "<h1>dbname: $dbname</h1>";
-    echo "<h1>username: $username</h1>";
-    echo "<h1>password: $password</h1>";
-  ?>
-
+    <h2>Hello, World!</h2>
+    <p>
+<?php
+    $conn = mysql_connect("$host:$port", $username, $password);
+    if($conn)
+    {
+      echo 'Connected successfully';
+      mysql_close($conn);
+    }
+?>
+    </p>
   </body>
 </html>
